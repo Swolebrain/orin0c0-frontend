b6e6795b5de29a999876d18900ba82c117d14dd3
Daniel Usani <daniel.e.usani@gmail.com>
2018-05-14 13:06:59 -0400

    Fixed the price chart to display cryptocurrency widget

--- src/components/Items.js
+++ src/components/Items.js
@@ -9,86 +9,42 @@ class Items extends Component {
           <div className="col">
             <h3 className="title text-center">Top Cryptocurrency Chart</h3>
 
-            <nav>
-              <div
-                className="nav nav-tabs nav-fill"
-                id="nav-tab"
-                role="tablist"
-              >
-                <a
-                  className="nav-item nav-link"
-                  id="nav-bitcoin-tab"
-                  data-toggle="tab"
-                  href="#nav-bitcoin"
-                  role="tab"
-                  aria-controls="nav-bitcoin"
-                  aria-selected="true"
-                >
-                  Bitcoin
-                </a>
-                <a
-                  className="nav-item nav-link active"
-                  id="nav-ethereum-tab"
-                  data-toggle="tab"
-                  href="#nav-ethereum"
-                  role="tab"
-                  aria-controls="nav-ethereum"
-                  aria-selected="false"
-                >
-                  Ethereum
-                </a>
-                <a
-                  className="nav-item nav-link"
-                  id="nav-litecoin-tab"
-                  data-toggle="tab"
-                  href="#nav-litecoin"
-                  role="tab"
-                  aria-controls="nav-litecoin"
-                  aria-selected="false"
-                >
-                  Litecoin
-                </a>
-              </div>
-            </nav>
-            <div className="tab-content" id="nav-tabContent">
-              <div
-                className="tab-pane fade"
-                id="nav-bitcoin"
-                role="tabpanel"
-                aria-labelledby="nav-bitcoin-tab"
-              />
-              <div
-                className="tab-pane fade show active"
-                id="nav-ethereum"
-                role="tabpanel"
-                aria-labelledby="nav-ethereum-tab"
-              >
-                <div
-                  className="coinmarketcap-currency-widget"
-                  data-currencyid="1"
-                  data-base="USD"
-                  data-secondary=""
-                  data-ticker="true"
-                  data-rank="true"
-                  data-marketcap="true"
-                  data-volume="true"
-                  data-stats="USD"
-                  data-statsticker="false"
-                />
-              </div>
-              <div
-                className="tab-pane fade"
-                id="nav-litecoin"
-                role="tabpanel"
-                aria-labelledby="nav-litecoin-tab"
-              >
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. A
-                ipsam, fugit obcaecati pariatur hic animi magnam sapiente, odit
-                laboriosam nam nesciunt nostrum at corrupti aspernatur! Nobis
-                enim explicabo accusantium aliquam. Quaerat culpa est quam
-                itaque! Vero iusto excepturi cum molestias?
-              </div>
-            </div>
+            <div
+              className="coinmarketcap-currency-widget"
+              data-currencyid="1"
+              data-base="USD"
+              data-secondary=""
+              data-ticker="true"
+              data-rank="true"
+              data-marketcap="true"
+              data-volume="true"
+              data-stats="USD"
+              data-statsticker="false"
+            />
+            <div
+              className="coinmarketcap-currency-widget"
+              data-currencyid="1027"
+              data-base="USD"
+              data-secondary=""
+              data-ticker="true"
+              data-rank="true"
+              data-marketcap="true"
+              data-volume="true"
+              data-stats="USD"
+              data-statsticker="false"
+            />
+            <div
+              className="coinmarketcap-currency-widget"
+              data-currencyid="2"
+              data-base="USD"
+              data-secondary=""
+              data-ticker="true"
+              data-rank="true"
+              data-marketcap="true"
+              data-volume="true"
+              data-stats="USD"
+              data-statsticker="false"
+            />
           </div>
           <div className="col">
             <h3 className="title text-center">Hot Items</h3>
