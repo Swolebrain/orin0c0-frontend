c8b9a34daba1017474f5e74397652a7d54a1e466
Daniel Usani <daniel.e.usani@gmail.com>
2018-05-16 20:01:25 -0400

    Added Redux to workflow

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,40 +1,44 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import { Provider } from 'react-redux'
 
 import Header from '../components/header'
 import Navbar from '../components/Navbar/Navbar'
+import store from '../store'
 
 import './fonts.css'
 import '../css/bootstrap.min.css'
 import './index.css'
 
 const Layout = ({ children, data }) => (
-  <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    >
-      <script
-        type="text/javascript"
-        src="https://files.coinmarketcap.com/static/widget/currency.js"
-      />
-    </Helmet>
-    <Navbar />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
+  <Provider store={store}>
+    <div>
+      <Helmet
+        title={data.site.siteMetadata.title}
+        meta={[
+          { name: 'description', content: 'Sample' },
+          { name: 'keywords', content: 'sample, something' },
+        ]}
+      >
+        <script
+          type="text/javascript"
+          src="https://files.coinmarketcap.com/static/widget/currency.js"
+        />
+      </Helmet>
+      <Navbar />
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 960,
+          padding: '0px 1.0875rem 1.45rem',
+          paddingTop: 0,
+        }}
+      >
+        {children()}
+      </div>
     </div>
-  </div>
+  </Provider>
 )
 
 Layout.propTypes = {
